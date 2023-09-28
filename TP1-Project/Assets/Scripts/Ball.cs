bf85258cf08eb146bcd1717e4041c95ed0b729d7
David2BD <109042558+David2BD@users.noreply.github.com>
2023-09-28 11:32:22 -0400

    Movement de la ball
    La balle se déplace et on peux set son accel et vitesse initial.

--- TP1-Project/Assets/Scripts/Ball.cs
+++ TP1-Project/Assets/Scripts/Ball.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public Vector3 acceleration = new Vector3(1.005f, -9.81f, 0f);
+    public Vector3 movementSpeed = new Vector3(0f,0f,0f);
+    private bool _moving = false;
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_moving == false)
+            {
+                _moving = true;
+            }
+            else
+            {
+                _moving = false;
+            }
+        }
+        if (_moving == true)
+        {
+            transform.position += movementSpeed * Time.deltaTime + (0.5f * acceleration * (Time.deltaTime*Time.deltaTime));
+            movementSpeed += acceleration * Time.deltaTime;
+        }
+
+        if (transform.position.x > 30 || transform.position.y > 30 || transform.position.z > 30)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- TP1-Project/Assets/Scripts/GameLoop.cs
+++ TP1-Project/Assets/Scripts/GameLoop.cs
@@ -15,4 +15,6 @@ public class GameLoop : MonoBehaviour
     {
         
     }
+    
+    
 }
