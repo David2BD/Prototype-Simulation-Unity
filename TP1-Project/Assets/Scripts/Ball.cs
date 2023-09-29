06edcca2a4c813e12999a9019f76bd2eaffd6958
David2BD <109042558+David2BD@users.noreply.github.com>
2023-09-28 22:28:47 -0400

    GameLoop
    Ont peut Left click pour spawner une ball et quand elle tombe assez loin du frame la prochaine peux spaner de l'autre coté.

--- TP1-Project/Assets/Scripts/Ball.cs
+++ TP1-Project/Assets/Scripts/Ball.cs
@@ -7,36 +7,26 @@ public class Ball : MonoBehaviour
 {
     public Vector3 acceleration = new Vector3(1.005f, -9.81f, 0f);
     public Vector3 movementSpeed = new Vector3(0f,0f,0f);
-    private bool _moving = false;
+    public GameLoop gameLoopScript;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameLoopScript = GameObject.FindObjectOfType(typeof(GameLoop)) as GameLoop;
     }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (_moving == false)
-            {
-                _moving = true;
-            }
-            else
-            {
-                _moving = false;
-            }
-        }
-        if (_moving == true)
-        {
-            transform.position += movementSpeed * Time.deltaTime + (0.5f * acceleration * (Time.deltaTime*Time.deltaTime));
-            movementSpeed += acceleration * Time.deltaTime;
-        }
 
-        if (transform.position.x > 30 || transform.position.y > 30 || transform.position.z > 30)
+        transform.position += movementSpeed * Time.deltaTime + (0.5f * acceleration * (Time.deltaTime*Time.deltaTime));
+        movementSpeed += acceleration * Time.deltaTime;
+
+        
+        if (transform.position.x < -20 || transform.position.y < -20 || transform.position.z < -20 ||
+            transform.position.x > 20 || transform.position.y > 20 || transform.position.z > 20)
         {
             Destroy(gameObject);
         }
+        
     }
 }
--- TP1-Project/Assets/Scripts/GameLoop.cs
+++ TP1-Project/Assets/Scripts/GameLoop.cs
@@ -1,20 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameLoop : MonoBehaviour
 {
+    public int playerTurn;
+    public GameObject ball;
+
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTurn = 1;
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if (!GameObject.FindGameObjectWithTag("Respawn"))
+        {
+            if (playerTurn == 1 && Input.GetMouseButtonDown(0))
+            {
+                Instantiate(ball, new Vector3(-5, 0, 0), Quaternion.identity);
+                playerTurn = 2;
+
+            }
+            else if (playerTurn == 2 && Input.GetMouseButtonDown(0))
+            {
+                Instantiate(ball, new Vector3(5, 0, 0), Quaternion.identity);
+                playerTurn = 1;
+            }
+        }
+
     }
     
+    public int GetplayerTurn()
+    {
+        return playerTurn;
+    }
     
+    public void SetplayerTurn(int player)
+    {
+        playerTurn = player;
+    }
 }
